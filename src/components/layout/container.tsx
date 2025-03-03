109868ff22eb278a8a25fa04b7212b6cf4c1f762
bernat-escarrà <bernat.escarra@gmail.com>
2025-03-03 01:55:04 +0100

    update Container component to use flexbox layout

--- src/components/layout/container.tsx
+++ src/components/layout/container.tsx
@@ -6,7 +6,7 @@ export type ContainerProps = ComponentProps<"div">;
 
 export const Container = ({ children, className, ...props }: ContainerProps) => {
 	return (
-		<div className={cn("container mx-auto px-6", className)} {...props}>
+		<div className={cn("container mx-auto flex flex-col px-6", className)} {...props}>
 			{children}
 		</div>
 	);
