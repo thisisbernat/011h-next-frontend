a596b1a91dc9bdf39024ea330b683ecb8916d1ae
bernat-escarrà <bernat.escarra@gmail.com>
2025-03-03 08:53:51 +0100

    refactor FilterForm to use nav element and add sorting buttons for improved accessibility and functionality

--- src/components/filter/filter-form.tsx
+++ src/components/filter/filter-form.tsx
@@ -1,4 +1,4 @@
-import { ListRestart } from "lucide-react";
+import { ArrowDownWideNarrow, ArrowUpNarrowWide, ListRestart } from "lucide-react";
 import React, { ComponentProps } from "react";
 
 import { cn } from "@/lib/utils";
@@ -7,16 +7,16 @@ import { ProductFilter } from "@/types";
 import { Button } from "../ui";
 
 import { FilterMultiSelect } from "./filter-multi-select";
-import { FilterSortButton } from "./filter-sort-button";
 import { FilterFormValues, useFilterParams } from "./use-filter-params";
 
 export interface FilterFormProps extends Omit<ComponentProps<"div">, "onSubmit"> {
 	onSubmit?: (values: FilterFormValues) => void;
 	className?: string;
+	containerClassName?: ComponentProps<"nav">["className"];
 	defaultValues?: Partial<FilterFormValues>;
 }
 
-export const FilterForm = ({ onSubmit, defaultValues, className, ...props }: FilterFormProps) => {
+export const FilterForm = ({ onSubmit, defaultValues, className, containerClassName, ...props }: FilterFormProps) => {
 	const {
 		filterValues: filterValues,
 		setCategoryParams,
@@ -31,33 +31,55 @@ export const FilterForm = ({ onSubmit, defaultValues, className, ...props }: Fil
 	});
 
 	return (
-		<div className={"flex gap-4"}>
-			<div className={cn("grid flex-grow grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-5", className)} {...props}>
+		<nav className={cn("flex gap-4 py-6", containerClassName)}>
+			<div
+				className={cn("grid w-full flex-grow grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-10", className)}
+				{...props}
+			>
 				<FilterMultiSelect
 					name={ProductFilter.Category}
 					value={filterValues[ProductFilter.Category]}
 					onChange={(values) => handleFilterChange(ProductFilter.Category, values, setCategoryParams)}
+					className={"col-span-full md:col-span-1 xl:col-span-2"}
 				/>
 				<FilterMultiSelect
 					name={ProductFilter.Type}
 					value={filterValues[ProductFilter.Type]}
 					onChange={(values) => handleFilterChange(ProductFilter.Type, values, setTypeParams)}
+					className={"col-span-full md:col-span-1 xl:col-span-2"}
 				/>
 				<FilterMultiSelect
 					name={ProductFilter.Color}
 					value={filterValues[ProductFilter.Color]}
 					onChange={(values) => handleFilterChange(ProductFilter.Color, values, setColorParams)}
+					className={"col-span-full md:col-span-1 xl:col-span-2"}
 				/>
 				<FilterMultiSelect
 					name={ProductFilter.Material}
 					value={filterValues[ProductFilter.Material]}
 					onChange={(values) => handleFilterChange(ProductFilter.Material, values, setMaterialParams)}
+					className={"col-span-full md:col-span-1 xl:col-span-2"}
 				/>
-				<FilterSortButton />
+				<Button variant={"outline"} className={"col-span-full md:col-span-1"}>
+					<ArrowUpNarrowWide />
+					<span>Price asc</span>
+				</Button>
+				<Button variant={"outline"} className={"col-span-full md:col-span-1"}>
+					<ArrowDownWideNarrow />
+					<span>Price desc</span>
+				</Button>
+				<Button
+					variant={"secondary"}
+					onClick={resetAllFilters}
+					className={"col-span-full flex w-full md:col-span-2 xl:hidden"}
+				>
+					<ListRestart />
+					<span>Reset all filters</span>
+				</Button>
 			</div>
-			<Button variant={"default"} onClick={resetAllFilters} className={"max-w-12"}>
+			<Button variant={"secondary"} onClick={resetAllFilters} className={"hidden max-w-12 xl:block"}>
 				<ListRestart />
 			</Button>
-		</div>
+		</nav>
 	);
 };
